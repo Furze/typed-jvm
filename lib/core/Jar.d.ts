0e57f6e8f76e9d25ca1820202d46db799d64f9d1
Troy <git@troycornwall.net>
2016-07-23 10:48:29 +1200

    Initial commit

---
+++ lib/core/Jar.d.ts
@@ -0,0 +1,18 @@
+declare var _createClass: (Constructor: any, protoProps: any, staticProps: any) => any;
+declare var _lodash: any;
+declare var _: any;
+declare var _ClassLoader: any;
+declare var _ClassCollection: any;
+declare function _interopRequireWildcard(obj: any): any;
+declare function _classCallCheck(instance: any, Constructor: any): void;
+declare var promisify: any;
+declare var fs: any;
+declare var AdmZip: any;
+/**
+ * Wrapper around a Java Archive file.
+ *
+ * Provides convenience methods for loading a Jar's classes.
+ *
+ * @see https://en.wikipedia.org/wiki/JAR_(file_format)
+ */
+declare var Jar: (file: any) => void;
